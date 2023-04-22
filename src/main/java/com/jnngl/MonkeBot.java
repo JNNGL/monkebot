c1824d63d3c16c33035e2b41e0d6ffc29c42bbce
jnngl <jnngles@gmail.com>
2023-04-22 19:19:15 +1100

    Use JavaCV

--- src/main/java/com/jnngl/MonkeBot.java
+++ src/main/java/com/jnngl/MonkeBot.java
@@ -1,8 +1,5 @@
 package com.jnngl;
 
-import com.ibasco.image.gif.GifFrame;
-import com.ibasco.image.gif.GifImageReader;
-import com.ibasco.image.gif.exceptions.InvalidSignatureException;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.OnlineStatus;
 import net.dv8tion.jda.api.entities.Activity;
@@ -12,12 +9,8 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 import net.dv8tion.jda.api.utils.FileUpload;
 import org.apache.commons.io.IOUtils;
-import org.jcodec.api.FrameGrab;
-import org.jcodec.api.JCodecException;
-import org.jcodec.api.PictureWithMetadata;
-import org.jcodec.common.io.ByteBufferSeekableByteChannel;
-import org.jcodec.common.io.SeekableByteChannel;
-import org.jcodec.scale.AWTUtil;
+import org.bytedeco.javacv.*;
+import org.bytedeco.javacv.Frame;
 import org.jetbrains.annotations.NotNull;
 
 import java.awt.*;
@@ -26,10 +19,8 @@ import java.awt.font.TextLayout;
 import java.awt.geom.AffineTransform;
 import java.awt.geom.Rectangle2D;
 import java.awt.image.BufferedImage;
-import java.awt.image.DataBufferInt;
 import java.io.*;
 import java.net.URL;
-import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -37,7 +28,7 @@ import java.util.*;
 import java.util.List;
 
 public class MonkeBot extends ListenerAdapter {
-
+    private static final FrameConverter<BufferedImage> FRAME_CONVERTER = new Java2DFrameConverter();
     private static final Font FONT;
 
     static {
@@ -73,85 +64,63 @@ public class MonkeBot extends ListenerAdapter {
 
     @Override
     public void onMessageReceived(@NotNull MessageReceivedEvent event) {
-        try (ByteArrayOutputStream outputStream = new ByteArrayOutputStream()) {
-            String message = event.getMessage().getContentRaw();
-            if (!message.startsWith("!monke")) {
-                return;
-            }
+        String message = event.getMessage().getContentRaw();
+        if (!message.startsWith("!monke")) {
+            return;
+        }
 
-            String[] args = message.split(" ", 2);
+        String[] args = message.split(" ", 2);
+        if (args.length == 1) {
+            event.getMessage().reply("Использование: !monke [ссылка] <текст>\n" +
+                    "Также можно ответить на сообщение с гифкой, в таком случае команда " +
+                    "выглядит так: !monke <текст>").queue();
+            return;
+        }
+
+        String text = args[1];
+        String url;
+
+        if (args[1].startsWith("https://") || args[1].startsWith("http://")) {
+            args = args[1].split(" ", 2);
             if (args.length == 1) {
-                event.getMessage().reply("Использование: !monke [ссылка] <текст>\n" +
-                        "Также можно ответить на сообщение с гифкой, в таком случае команда " +
-                        "выглядит так: !monke <текст>").queue();
+                event.getMessage().reply("Текст не указан.").queue();
                 return;
             }
 
-            String text = args[1];
-            String url;
+            url = args[0];
+            text = args[1];
+        } else if (event.getMessage().getReferencedMessage() != null) {
+            Message referenced = event.getMessage().getReferencedMessage();
+            url = getAttachmentUrl(referenced).orElseGet(() -> event.getMessage().getReferencedMessage().getContentRaw());
+        } else if (!event.getMessage().getAttachments().isEmpty()) {
+            url = getAttachmentUrl(event.getMessage()).orElse("");
+        } else {
+            event.getMessage().reply("Ссылка на гифку не указана.").queue();
+            return;
+        }
 
-            if (args[1].startsWith("https://") || args[1].startsWith("http://")) {
-                args = args[1].split(" ", 2);
-                if (args.length == 1) {
-                    event.getMessage().reply("Текст не указан.").queue();
-                    return;
-                }
+        if (!url.startsWith("https://") && !url.startsWith("http://")) {
+            event.getMessage().reply("Указана неверная ссылка.").queue();
+            return;
+        }
 
-                url = args[0];
-                text = args[1];
-            } else if (event.getMessage().getReferencedMessage() != null) {
-                Message referenced = event.getMessage().getReferencedMessage();
-                url = getAttachmentUrl(referenced).orElseGet(() -> event.getMessage().getReferencedMessage().getContentRaw());
-            } else if (!event.getMessage().getAttachments().isEmpty()) {
-                url = getAttachmentUrl(event.getMessage()).orElseThrow(() -> new IOException("але ты не гифку скинул"));
-            } else {
-                event.getMessage().reply("Ссылка на гифку не указана.").queue();
-                return;
-            }
-
-            if (!url.startsWith("https://") && !url.startsWith("http://")) {
-                event.getMessage().reply("Указана неверная ссылка.").queue();
-                return;
-            }
-
-            if (url.contains("//tenor.com/")) {
+        if (url.contains("//tenor.com/")) {
+            try {
                 String html = IOUtils.toString(new URL(url), StandardCharsets.UTF_8);
                 url = html.split("meta itemProp=\"contentUrl\" content=\"", 2)[1].split("\"", 2)[0];
+            } catch (IOException e) {
+                return;
             }
+        }
 
-            List<BufferedImage> frames = new ArrayList<>();
-            List<Integer> delays = new ArrayList<>();
-            int width = 0;
-            int height = 0;
-
-            try {
-                GifImageReader reader = new GifImageReader(new URL(url).openStream());
-                while (reader.hasRemaining()) {
-                    GifFrame frame = reader.read();
-                    BufferedImage image = new BufferedImage(frame.getWidth(), frame.getHeight(), BufferedImage.TYPE_INT_ARGB);
-                    System.arraycopy(frame.getData(), 0, ((DataBufferInt) image.getRaster().getDataBuffer()).getData(), 0, frame.getWidth() * frame.getHeight());
-                    delays.add(frame.getDelay() * 10);
-                    frames.add(image);
-                    width = Math.max(width, image.getWidth());
-                    height = Math.max(height, image.getHeight());
-                }
-            } catch (InvalidSignatureException e) {
-                byte[] bytes = IOUtils.toByteArray(new URL(url));
-                SeekableByteChannel channel = new ByteBufferSeekableByteChannel(ByteBuffer.wrap(bytes), bytes.length);
-                FrameGrab frameGrab = FrameGrab.createFrameGrab(channel);
-                PictureWithMetadata picture;
-                while ((picture = frameGrab.getNativeFrameWithMetadata()) != null) {
-                    frames.add(AWTUtil.toBufferedImage(picture.getPicture()));
-                    delays.add((int) (picture.getDuration() * 1000));
-                    width = picture.getPicture().getWidth();
-                    height = picture.getPicture().getHeight();
-                }
-            }
-
+        try (ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+             FFmpegFrameGrabber grabber = new FFmpegFrameGrabber(new URL(url).openStream())) {
             List<String> lines = Arrays.asList(text.split("\n"));
             Collections.reverse(lines);
 
-            BufferedImage textImage = new BufferedImage(width, height, 2);
+            grabber.start();
+
+            BufferedImage textImage = new BufferedImage(grabber.getImageWidth(), grabber.getImageHeight(), BufferedImage.TYPE_INT_ARGB);
             Graphics2D textGraphics = textImage.createGraphics();
             textGraphics.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
             textGraphics.setFont(FONT);
@@ -199,15 +168,18 @@ public class MonkeBot extends ListenerAdapter {
             encoder.start(outputStream);
             encoder.setRepeat(0);
             encoder.setSize(textImage.getWidth(), textImage.getHeight());
+            encoder.setDelay((int) (1000 / grabber.getFrameRate()));
 
-            for (int i = 0; i < frames.size(); i++) {
-                BufferedImage image = frames.get(i);
+            Frame frame;
+            while ((frame = grabber.grabFrame(false, true, true, false, false)) != null) {
+                BufferedImage image = FRAME_CONVERTER.convert(frame);
                 Graphics2D graphics = image.createGraphics();
                 graphics.drawImage(textImage, 0, 0, null);
                 graphics.dispose();
                 encoder.addFrame(image);
-                encoder.setDelay(delays.get(i));
             }
+
+            grabber.stop();
             encoder.finish();
 
             byte[] bytes = outputStream.toByteArray();
@@ -218,7 +190,7 @@ public class MonkeBot extends ListenerAdapter {
                 IOUtils.write(bytes, new FileOutputStream("data/monke/" + filename));
                 event.getMessage().reply("https://api.jnngl.me/monke/" + filename).queue();
             }
-        } catch (IOException | JCodecException e) {
+        } catch (IOException e) {
             event.getMessage().reply("Не получилось добавить текст на говногифку: " + e.getMessage()).queue();
         }
     }
